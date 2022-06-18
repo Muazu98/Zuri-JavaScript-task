b0647f453a313bdf46dc82af0990fbc159759942
Muazu98 <zakarimuazu98@gmail.com>
2022-06-17 21:24:17 -0700

    My first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const names = document.querySelector('.name')
+const height = document.querySelector('.height')
+const country = document.querySelector('.country')
+
+names.appendChild(document.createTextNode(' Name: Muazu Zakari'))
+height.appendChild(document.createTextNode('Height: 5 foot 9'))
+country.appendChild(document.createTextNode('Country: Nigeria'))
